855730c54316a8b1dba0e2ccfd99b38dc7d60919
Deepali123verma <166192241+Deepali123verma@users.noreply.github.com>
2025-07-24 15:41:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,16 @@ from config_db import Config
 from routes import routes
 
 app = Flask(__name__)
-app.config.from_object(Config)
+
+# JWT Configuration
+app.config["JWT_ALGORITHM"] = Config.JWT_ALGORITHM
+app.config["JWT_PRIVATE_KEY"] = Config.JWT_PRIVATE_KEY
+app.config["JWT_PUBLIC_KEY"] = Config.JWT_PUBLIC_KEY
 
 jwt = JWTManager(app)
 
+# Register routes
 app.register_blueprint(routes)
-
-@app.route('/')
-def home():
-    return "Flask App with JWT is Running!"
 
 if __name__ == '__main__':
     app.run(debug=True)
