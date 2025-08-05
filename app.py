153b5ae0e870e926f092edecb209a8e6b589454d
Deepali123verma <166192241+Deepali123verma@users.noreply.github.com>
2025-08-05 18:21:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
-from flask import Flask
-from flask_jwt_extended import JWTManager
 from config_db import Config
 from routes import routes
 
+#  Create an instance of Config
+config = Config()
+
 app = Flask(__name__)
 
-# JWT Configuration
-app.config["JWT_ALGORITHM"] = Config.JWT_ALGORITHM
-app.config["JWT_PRIVATE_KEY"] = Config.JWT_PRIVATE_KEY
-app.config["JWT_PUBLIC_KEY"] = Config.JWT_PUBLIC_KEY
+#  Set Flask config values using the instance attributes
+app.config["SECRET_KEY"] = config.SECRET_KEY
+app.config["JWT_ALGORITHM"] = config.JWT_ALGORITHM
+app.config["JWT_PRIVATE_KEY"] = config.JWT_PRIVATE_KEY
+app.config["JWT_PUBLIC_KEY"] = config.JWT_PUBLIC_KEY
 
 jwt = JWTManager(app)
 
-# Register routes
 app.register_blueprint(routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
