546cd23a9a2b2a729bb7b85cf1d9ed115bbd12c0
Deepali123verma <166192241+Deepali123verma@users.noreply.github.com>
2026-02-20 09:42:22 +0530

    Refactor app.py to load configuration from Config class

--- app.py
+++ app.py
@@ -4,25 +4,24 @@ from config_db import Config, db
 from routes import routes
 from flasgger import Swagger
 
+
 app = Flask(__name__)
 
-# JWT Configuration
-app.config["JWT_ALGORITHM"] = Config.JWT_ALGORITHM
-app.config["JWT_PRIVATE_KEY"] = Config.JWT_PRIVATE_KEY
-app.config["JWT_PUBLIC_KEY"] = Config.JWT_PUBLIC_KEY
+# Load ALL configuration from Config class
+app.config.from_object(Config)
 
+# Initialize extensions
+db.init_app(app)
 jwt = JWTManager(app)
 
-# SQLAlchemy Configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = Config.SQLALCHEMY_DATABASE_URI
-db.init_app(app)
-
+# Create tables
 with app.app_context():
     db.create_all()
+
 # Swagger Configuration
 swagger_template = {
     "info": {
-        "title": "Task Management REST API",
+        "title": "Task Management System",
         "description": "API documentation for the Task Management project with JWT authentication, role-based authorization, and encryption.",
         "version": "1.0.0",
     },
@@ -44,4 +43,3 @@ app.register_blueprint(routes)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
